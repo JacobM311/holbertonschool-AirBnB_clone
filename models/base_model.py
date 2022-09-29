4f1923c5f8653b0ce1b78f1e3bab5fc3a270e9ba
root <4908@holbertonschool.com>
2022-09-29 12:51:01 -0700

    f

--- models/base_model.py
+++ models/base_model.py
@@ -12,28 +12,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Base Model __init__ Method"""
-<<<<<<< HEAD
 
-=======
->>>>>>> b5b332daa2be1650e51b687ba8d6efcaa4d8b1c2
         self.id = str(uuid4())
         self.name = name
         self.updated_at = self.created_at = datetime.now()
 
     def __str__(self):
         """prints string representation of base"""
-<<<<<<< HEAD
 
     return "[{}] ({}) {}".format(self.__class__.__name__,
                                  self.id, self.__dict__)
-=======
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-<<<<<<< HEAD
-                                self.id, self.__dict__)
->>>>>>> b5b332daa2be1650e51b687ba8d6efcaa4d8b1c2
-=======
-                                     self.id, self.__dict__)
->>>>>>> 8b4165f40e2d0f4cbae82ba5c06a1bfbab5b5ff5
 
     def save(self):
         """saves"""
