4ba4d81b8ef2bcaa8c9cc7461de6dc96ba44c367
root <4908@holbertonschool.com>
2022-09-29 12:36:00 -0700

    file cleanup

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,7 @@
 """base model"""
 
 
-<<<<<<< HEAD
-=======
 import json
->>>>>>> 0bce6b3e778025d8430e7bd65ad14cf28460cd56
 from uuid import uuid4
 from datetime import datetime
 
@@ -15,26 +12,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Base Model __init__ Method"""
-<<<<<<< HEAD
+
 
         self.id = str(uuid4())
         self.name = name
         self.updated_at = self.created_at = datetime.now()
 
-=======
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
->>>>>>> 0bce6b3e778025d8430e7bd65ad14cf28460cd56
-
     def __str__(self):
         """prints string representation of base"""
 
-<<<<<<< HEAD
     return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-=======
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
->>>>>>> 0bce6b3e778025d8430e7bd65ad14cf28460cd56
 
     def save(self):
         """saves"""
