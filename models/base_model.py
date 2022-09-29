e0c5108abf418f8273d3ed9ff7c12701c7d59eb4
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-29 12:40:17 -0700

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -12,25 +12,20 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Base Model __init__ Method"""
-
-
         self.id = str(uuid4())
         self.name = name
         self.updated_at = self.created_at = datetime.now()
 
     def __str__(self):
         """prints string representation of base"""
-
-    return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """saves"""
-
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """dictionary"""
-
         return dict(self.__dict__,
                     __class__=self.__class__.__name__,
                     updated_at=self.updated_at.isoformat(),
