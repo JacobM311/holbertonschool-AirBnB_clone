a7c923cd7f41c3e254c09ed4742a854ee232f9da
root <4908@holbertonschool.com>
2022-09-29 14:27:07 -0700

    tests so far

---
+++ tests/test_base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" unit tests for BaseModel class """
+import inspect
+import models
+import pep8
+from models import base_model
+from models.base_model import BaseModel
+import unittest
+from datetime import datetime, time
+
+class TestBaseModelDocs(unittest.TestCase):
+    """ tests for basemodel """
+
+
+
+    def test_ModuleDocstring(self):
+        """ tests for docstring """
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+    def test_FuncDocstrings(self):
+        """ tests for docstrings """
+        self.assertTrue(len(BaseModel.__init__.__doc__) >= 1)
+        self.assertTrue(len(BaseModel.__str__.__doc__) >= 1)
+        self.assertTrue(len(BaseModel.save.__doc__) >= 1)
+        self.assertTrue(len(BaseModel.to_dict.__doc__) >= 1)
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
