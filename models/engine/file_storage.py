3791bd88eee5bd26c534d7936b2101a29f6dc234
JacobM311 <jacobmarshall9608@yahoo.com>
2022-10-01 14:10:29 -0700

    adding file_storage.py and engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,7 +21,8 @@ class FileStorage:
 
     def new(self, obj):
         "sets in __objects the obj with key <obj class name>.id"
-        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
