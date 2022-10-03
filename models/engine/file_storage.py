2d36a6753d2ba1227004e4afd7b04972b3ee8c2f
JacobM311 <jacobmarshall9608@yahoo.com>
2022-10-03 12:15:14 -0700

    updating file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,12 +24,12 @@ class FileStorage:
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        with open(self.__file_path, 'w', encoding='utf-8') as outinstances:
+        with open(self.__file_path, mode="w", encoding="utf-8") as f:
             json.dump(
                 {
                     k: (v.to_dict() if not isinstance(v, dict) else v)
                     for (k, v) in self.__objects.items()
-                }, outinstances)
+                }, f)
 
     def reload(self):
         """deserializes the JSON file to __objects
@@ -37,9 +37,5 @@ class FileStorage:
         ; otherwise, do nothing. If the file doesn’t exist,
         no exception should be raised)"""
         if exists(self.__file_path):
-            with open(self.__file_path, encoding='utf-8') as ininstances:
-                old_instances = json.load(ininstances)
-                for key in old_instances:
-                    self.__objects[key] = getattr(
-                        models,
-                        old_instances[key]['__class__'])(**old_instances[key])
+            with open(self.__file_path, encoding='utf-8') as f:
+                self.__objects = json.load(f)
